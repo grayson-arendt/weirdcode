e47abd57e3e74c7b377b9ba0f61d744ab2fdf26c
Grayson Arendt <97147107+grayson-arendt@users.noreply.github.com>
2022-06-09 15:34:09 -0500

    Update targetstocks.py

--- targetstocks.py
+++ targetstocks.py
@@ -6,12 +6,12 @@ from bokeh.io import curdoc
 from bokeh.plotting import figure, output_file, show
 
 data = pd.read_csv('C:/Users/Grayson/Dropbox/PC/Downloads/tgt_price-history-06-09-2022 (1).csv')
-data.drop(data.index[data['Time'] == 0], inplace = True)
+data.drop(data.index[data['Time'] == 0],inplace=True)
 
 output_file("tgt.html")
 curdoc().theme = 'dark_minimal'
 
-graph = figure(x_axis_type= "datetime",title = "Target Stocks (2020-2022)",height=350,width=600)
+graph = figure(x_axis_type="datetime",title="Target Stocks (2020-2022)",height=350,width=600)
 
 graph.xaxis.axis_label = "Date"
 graph.yaxis.axis_label = "Price (in USD)"
